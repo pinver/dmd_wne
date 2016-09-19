4fdac5665e713d441a2a1b80194f4a2729a080d0
Paolo Invernizzi <paolo.invernizzi@gmail.com>
2016-09-19 16:11:17 +0200

    Digital Mars compiler 2.071.2 was released.

---
+++ dmd_2.071.2/src/phobos/std/c/string.d
@@ -0,0 +1,16 @@
+// @@@DEPRECATED_2017-06@@@
+
+/**
+ * $(RED Deprecated. Use $(D core.stdc.string) instead. This module will be
+ *       removed in June 2017.)
+ *
+ * C's &lt;string.h&gt;
+ * Authors: Walter Bright, Digital Mars, http://www.digitalmars.com
+ * License: Public Domain
+ * Macros:
+ *      WIKI=Phobos/StdCString
+ */
+deprecated("Import core.stdc.string instead")
+module std.c.string;
+
+public import core.stdc.string;
